03d8919c28cfd226b88a7bcaf4edcd3a5d7751d8
Add-ame <simoaddame17@gmail.com>
2025-01-27 06:52:40 +0100

    learning...

--- pipex.c
+++ pipex.c
@@ -4,19 +4,27 @@
 Check if infile exist or not
 if it exist open it in read mode
 */
-/*
+
+void	free_pt(char **pt)
+{
+	int	i;
+
+	i = 0;
+	while (pt && pt[i])
+		free(pt[i++]);
+	free(pt);
+}
+
 char	*check_cmd_env(char *argv, char **env)
 {
-	int		i;
-	int		j;
+	int		i, j, finded;
 	char	**pt;
-	char	*path;
-	char	cmp[6];
-	int		finded;
+	char	*cmp, *cmd;
+	char	*tmp;
 
 	i = 0;
 	finded = 0;
-	cmp[6] = "PATH=";
+	cmp = "PATH=";
 	while (env[i])
 	{
 		j = 0;
@@ -29,87 +37,98 @@ char	*check_cmd_env(char *argv, char **env)
 		}
 		i++;
 	}
-
-	if (!finded)
+	if (!ft_strlen(argv) || !c_w(argv, ' '))
 		return (NULL);
-	pt = ft_split(env[i] + 4, ':'); // 4 | 5
-	
-}*/
+	pt = ft_split(env[i] + 5, ':');
+	finded = 0;
+	i = 0;
+	while (pt[i])
+	{
+		tmp = pt[i];
+		pt[i] = ft_strjoin(tmp, "/");
+		free(tmp);
+		cmd = ft_strjoin(pt[i], argv);
+		if (access(cmd, F_OK | X_OK) == 0)
+		{
+			finded = 1;
+			break;
+		}
+		free(cmd);
+		i++;
+	}
+	if (!finded)
+		return (free_pt(pt), NULL);
+	return (pt[i]);
+}
+
+/**
+ * 1- open infile
+ * 2- check if cmd exist in env path
+ * 3- execute the cmd 
+ * 4- fork for 2 cmd
+ */
 
 int     main(int ac, char **av, char **env)
 {
-	int		fd[2];
-	int		pid1;
-	int		pid2;
-	int		fd1;
-	int		fd2;
-	int		i;
-	int		j;
-	char	**argv;
-	char	*cmd1;
-	char	*cmd2;
-	int		wait_status;
-	int		status_code;
+	int		pid_1, pid_2;
+	int		i, j;
+	int		fd[2], fd1, fd2;
+	int		wait_status, status_code;
+	char	*cmd_1, *cmd_2, **argv;
 
-
+	pipe(fd);
 	fd1 = open(av[1], O_RDONLY);
-	pipe(fd);// fd[0] fd[1]
-	pid1 = fork();
-	if (pid1 == 0)
+	pid_1 = fork();
+	if (pid_1 == 0)
 	{
 		dup2(fd1, 0);
 		dup2(fd[1], 1);
 		close(fd[0]);
 		close(fd[1]);
+		// close(fd[0]);
+		// take the cmd and check if you have access...
 		argv = ft_split(av[2], ' ');
-		// cmd1 = check_cmd_env(argv[0], env);
-		cmd1 = ft_strjoin("/usr/bin/", argv[0]);
-		if (access(cmd1, F_OK | X_OK) == -1)
+		cmd_1 = check_cmd_env(argv[0], env);
+		if (!cmd_1)
 		{
-			write(2, "Command not found\n", 18);
+			write(2, "Cmd not found in env path\n", 26);
+			free_pt(argv);
 			exit(127);
 		}
-		if (execve(cmd1, argv, NULL) == -1)
-		{
-			perror(NULL);
-			exit (errno); // temporary
-		}
-		// the output of execve should be in fd[1] and next cmd should take that as input
-		// dup(fd[1], 1);
+		cmd_1 = ft_strjoin(cmd_1, argv[0]);
+		execve(cmd_1, argv, NULL);
 	}
 	else
 		close(fd1);
+	waitpid(pid_1, NULL, 0);
 
 	fd2 = open(av[4], O_CREAT | O_WRONLY, 0777);
-	pid2 = fork();
-	if (pid2 == 0)
+	pid_2 = fork();
+	if (pid_2 == 0)
 	{
 		dup2(fd[0], 0);
 		dup2(fd2, 1);
 		close(fd[0]);
 		close(fd[1]);
+		// close(fd[1]);
+		// take the cmd and check if you have access...
 		argv = ft_split(av[3], ' ');
-		// cmd1 = check_cmd_env(argv[0], env);
-		cmd2 = ft_strjoin("/usr/bin/", argv[0]);
-		if (access(cmd2, F_OK | X_OK) == -1)
+		cmd_2 = check_cmd_env(argv[0], env);
+		if (!cmd_2)
 		{
-			write(2, "Command not found\n", 18);
+			write(2, "Cmd not found in env path\n", 26);
+			free_pt(argv);
 			exit(127);
 		}
-		if (execve(cmd2, argv, NULL) == -1)
-		{
-			perror(NULL);
-			exit (errno); // temporary
-		}
-		// the output of execve should be in fd[1] and next cmd should take that as input
-		// dup(fd[1], 1);
+		cmd_2 = ft_strjoin(cmd_2, argv[0]);
+		execve(cmd_2, argv, NULL);
 	}
 	else
 		close(fd2);
+
 	close(fd[0]);
 	close(fd[1]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, &status_code, 0);
+	wait(&wait_status);
 	status_code = WEXITSTATUS(wait_status);
 	exit(status_code);
 	return (0);
